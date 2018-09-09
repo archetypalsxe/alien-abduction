7a41b8ab14258c3b6324fb5b4c9843edc0edecff
Jack Ross, III <sxe4ever@gmail.com>
2018-09-09 10:52:15 -0400

    Removing unused scripts, hooking main menu up to the game

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,35 +1,25 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-abstract public class SceneController : MonoBehaviour {
-
-	/**
-	 * Text saying to press any key to begin
-	 */
-	public Text subText;
-
-	/**
-	 * Whether or not pressing a key will have any affect
-	 */
-	protected bool ready = false;
-
-	// Abstract method to mandate that the time to wait is set
-	abstract protected int getTimeToWait();
-
-	// Returns the subtext that should be displayed after the delay
-	abstract protected string getSubText();
+public class SceneController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		subText.text = "";
-		IEnumerator coroutine = this.waitSomeTime(this.getTimeToWait());
-		StartCoroutine(coroutine);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 
-  protected IEnumerator waitSomeTime(int seconds) {
-      yield return new WaitForSeconds(seconds);
-			this.subText.text = this.getSubText();
-      this.ready = true;
+  public void repeatLevel() {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void nextLevel() {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   }
 }
