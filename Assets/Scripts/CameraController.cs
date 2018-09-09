ed87ed71583cbd168d163afc811760cad2258b0c
John Li <johnliglobal@gmail.com>
2018-09-09 11:19:47 -0400

    Objects fade between camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,21 +13,45 @@ public class CameraController : MonoBehaviour {
     /**
      * Used for initialization
      */
-	void Start ()
+    void Start()
     {
         offsetValue = transform.position - player.transform.position;
         startingScale = player.transform.localScale;
-	}
-	
+        StartCoroutine(SeeThroughObjectsDetectionRoutine());
+    }
+
     /**
      * Called once per frame, but runs last after all other items are updated.
      * Ensures that the player has already moved
      */
-	void Update ()
+    void Update()
     {
         Vector3 difference = player.transform.localScale - startingScale;
         transform.position = player.transform.position + offsetValue + new Vector3(
             0, difference.x / (distanceMultiplier * distanceDivisor), -(difference.x / distanceMultiplier)
         );
-	}
+    }
+
+    IEnumerator SeeThroughObjectsDetectionRoutine()
+    {
+        const float tick = .5f;
+        Vector3 target;
+        while (true)
+        {
+            target = player.transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, (target - transform.position).normalized, out hit) && 
+                hit.rigidbody != null &&
+                hit.rigidbody.tag.Equals("Pick Up"))
+            {
+                BlockCameraFade fader = hit.rigidbody.gameObject.GetComponent<BlockCameraFade>();
+                if(fader == null)
+                {
+                   fader = hit.rigidbody.gameObject.AddComponent<BlockCameraFade>();
+                }
+                fader.hitByCamera();
+            }
+            yield return new WaitForSeconds(tick);
+        }
+    }
 }
