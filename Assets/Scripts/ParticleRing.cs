6a0c4352ea2ec05ff454412b8520d476c07e1142
Jack Ross, III <sxe4ever@gmail.com>
2018-09-09 14:09:06 -0400

    Adding particle effects and making player transparent

---
+++ Assets/Scripts/ParticleRing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRing : MonoBehaviour {
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(.5f * player.localScale.x, 
+            .5f * player.localScale.x,
+            .5f * player.localScale.z );
+    }
+}
